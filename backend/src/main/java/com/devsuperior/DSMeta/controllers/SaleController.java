46906e9e0f04b406364a145e680610a5798d053c
esadorinha <isa.starco.schwaab@gmail.com>
2022-07-17 14:38:28 -0300

    problema

--- backend/src/main/java/com/devsuperior/DSMeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/DSMeta/controllers/SaleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.DSMeta.entities.Sale;
 import com.devsuperior.DSMeta.services.SaleService;
+import com.devsuperior.DSMeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -18,11 +19,19 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping
 	public Page<Sale> findSales(
 			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
 			@RequestParam(value = "maxDate", defaultValue = "")String maxDate, 
 			Pageable pageable) {
 		return service.findSales(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping("/notification")
+	public void notifySms() {
+		smsService.sendSms();
 	}
 }
